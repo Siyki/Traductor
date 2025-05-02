e39c5e0bc3e5d8ebf87128b9bd9c3bdb21ec9026
Siyki <isabeta.mar@gmail.com>
2025-05-01 23:55:21 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -15,7 +15,7 @@ st.markdown(
     """
     <style>
         html, body, .stApp {
-            background-color: #e0f7fa !important;
+            background-color: #e0f7ec !important;
         }
         h1, h2, h3, h4, p {
             text-align: center;
@@ -54,7 +54,7 @@ st.markdown(
 st.markdown('<h1>🌐 Traductor Mágico 🎧</h1>', unsafe_allow_html=True)
 st.markdown('<h3>🎙️ Habla y nosotros lo traducimos con amor 💌</h3>', unsafe_allow_html=True)
 
-image = Image.open('OIG7.jpg')
+image = Image.open('lengua.jpg')
 st.image(image, width=300)
 
 st.markdown("""
